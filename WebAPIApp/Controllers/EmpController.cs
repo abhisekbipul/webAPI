b0bca84df4585e9e35c57c5d541fce91c295e8c0
Abhisek Patra <patraabhisek.82@gmail.com>
2024-09-03 17:04:48 +0530

    updated

--- WebAPIApp/Controllers/EmpController.cs
+++ WebAPIApp/Controllers/EmpController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using WebAPIApp.Data;
 using WebAPIApp.Models;
 
@@ -23,7 +24,15 @@ namespace WebAPIApp.Controllers
             db.SaveChanges();
             return Ok("Emp Added successfully");
         }
+        [Route("AddEmpList")]
+        [HttpPost]
+        public IActionResult AddEmpList(List<Emp> empList)
+        {
+            db.employee.AddRange(empList);
+            db.SaveChanges();
+            return Ok("EmpList Added successfully");
 
+        }
         [Route("GetEmp")]
         [HttpGet]
         public IActionResult GetEmp()
@@ -41,9 +50,52 @@ namespace WebAPIApp.Controllers
             return Ok("Employee Deleted Successfully");
         }
 
-        public IActionResult updateEmpById()
+        [Route("DeleteEmpList")]
+        [HttpDelete]
+        public IActionResult DeleteMultiple(List<int> id)
         {
+            var data=db.employee.Where(x=>id.Contains(x.Id)).ToList();
+            db.employee.RemoveRange(data);
+            db.SaveChanges();
+            return Ok("Employee Deleted Successfully");
+        }
 
+        [Route("UpdateEmployee")]
+        [HttpPut]
+        public IActionResult updateEmp([FromForm]Emp emp)
+        {
+            db.employee.Update(emp);
+            db.SaveChanges();
+            return Ok("Employee updated successfully");
+        }
+
+        [Route("UpdateEmp/{id}")]
+        [HttpPut]
+        public IActionResult updateEmpById(int id,Emp updatedEmp)
+        {
+            var existingEmp = db.employee.Find(id);
+            if (existingEmp == null)
+            {
+                return NotFound("Employee not found");
+            }
+
+            
+            existingEmp.Name = updatedEmp.Name;
+            existingEmp.Department = updatedEmp.Department;
+            existingEmp.Salary = updatedEmp.Salary;
+            
+            db.SaveChanges();
+
+            return Ok("Employee updated successfully");
+
+        }
+        [Route("SearchEmp/{search}")]
+        [HttpGet]
+        public IActionResult searchEmp(string search)
+        {
+            
+            var data=db.employee.Where(x=>search.Contains(x.Name)||search.Contains(x.Department)|| search.Contains(x.Salary.ToString())).ToList();
+            return Ok(data);
         }
     }
 }
--- WebAPIApp/obj/Debug/net8.0/WebAPIApp.AssemblyInfo.cs
+++ WebAPIApp/obj/Debug/net8.0/WebAPIApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3023febb0dd0959bb7a79f1209b84406e9c1b541")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPIApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPIApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
